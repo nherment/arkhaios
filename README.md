48b1e8855a79f3cac7681f59f42829358926c7d1
Nicolas Herment <nherment@Nicolass-MacBook-Pro.local>
2013-08-27 06:31:16 +0200

    a bit of refactoring, consolidated docs in the readme

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ The sources are full of hacks.
 
 ![Arkhaios screenshot](http://docs.arkhaios.net/assets/screenshot.jpg "Arkhaios screenshot")
 
+Information
+===========
+
+* Sources https://github.com/nherment/arkhaios
+* Documentation: http://docs.arkhaios.net
+* Code Analysis: http://docs.arkhaios.net/report
+* Live Demo: http://arkhaios.net
+
 
 First Steps
 ===========
@@ -41,11 +49,148 @@ Set up the admin password
 
 The first login will set the administrator password. To do so, navigate to your app at http://localhost:3000/admin.
 
-Information
-===========
+Options
+=======
 
-* Sources https://github.com/nherment/arkhaios
-* Documentation: http://docs.arkhaios.net
-* Code Analysis: http://docs.arkhaios.net/report
-* Live Demo: http://arkhaios.net
-* License: MIT (see license file)
+
+Http Port
+---------
+
+You can customize the port used by the webserver:
+
+    export PORT=3000
+
+
+Configuration:
+
+The following are environment variables that you can use to customize Arkhaios.
+
+    export ARKHAIOS_TITLE="My Website Title"  // website's title
+
+    export ARKHAIOS_GA_UA="UA-43216-1"        // Google Analytics UA
+    export ARKHAIOS_GA_HOST="localhost"       // Google Analytics host name
+
+    export ARKHAIOS_FORK_ME="https://github.com/nherment/arkhaios" // fork me ribbon and it's URL
+
+
+Troubleshooting
+===============
+
+Uploading files fails
+---------------------
+
+Try again without a proxy (eg. nginx or apache).
+
+If it works, your proxy configuration may have a request timeout or a body size limit set.
+For example, NGINX has low default limits (with reason !). Namely ```send_timeout``` and ```client_max_body_size```.
+
+Help
+----
+
+For help, contact with through issues here: https://github.com/nherment/arkhaios/issues
+
+How to administer Arkhaios
+==========================
+
+Administration URL
+------------------
+
+The http address to access the administration of Arkhaios is ```/admin```. If you are accessing your application from
+```localhost```, the link to access it is http://localhost:3000/admin.
+
+Logging in
+----------
+
+Before gaining access to the administration, you will need to be logged in. The first login will set the password.
+Following logins will require you to enter the password that you entered the first time.
+
+The password is stored in the DB as a un-salted SHA-512 hash.
+
+Administration Menu
+-------------------
+
+The administration consists in the following menus:
+
+- Upload
+- Images
+- Access Control
+
+There is another menu item which allows you to logout
+
+### Upload
+
+This menu allows you to upload JPEG and PNG images. Other format are not supported yet.
+
+By default, uploaded images are not tied to any tag. You can attach them to the ** Public ** tag to make them visible by
+anonymous visitors.
+
+### Images
+
+Here you can rename uploaded images and attach them to tags.
+Tags identify images and gives you great control to share them.
+
+### Access Control
+
+Access Controls (aka ACLs) are the best way to share your photos.
+
+Each ACL is tied to one or several tags and generates a unique link. You can give this link to anyone. The links adds
+up. This mean that if you give 2 ACLs links to one person, this person will have access to whatever either links gives
+him access to.
+
+There is a default ACL that you never see. It allows any visitor to see images tied to the ** Public ** tag.
+
+#### Sharing examples
+
+*Example 1*
+
+I have two tags, ** Family ** and ** Friends **. I create one ACL for each tag and give one to my friends and the other one
+to my parent. I give my wife both links so that she can see all the pictures I make.
+
+*Example 2*
+
+I create as many tags as I do activities. For example, last week-end was my mother's birthday so I create a tag called
+** 2013 Mom Birthday **. This tag is shared with anyone that was present.
+
+If everytime I do an activity I create a specific tag, I can choose to just share this tag with whoever was present.
+
+TODOs
+=====
+
+Unordered list of things on my plate.
+
+- refactor the code (crappy all over)
+- cookie for 'remember me' on access controls
+- installation wizard
+- show detail of image
+- show image name on mouse hover
+- capability to add description to pictures
+- batch edit of pictures
+- tags autocomplete
+- add tags on image upload
+- use Redis as memory store
+- automatically convert tiff to jpeg and work as an archiving mecanism
+- span multiple servers (either smart resolution of images file paths or shared storage bay or amazon S3...)
+- multi-tenancy
+
+The MIT License (MIT)
+=====================
+
+Copyright (c) 2013 Nicolas Herment
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- server.js
+++ server.js
@@ -1,34 +1,52 @@
 var express         = require("express")
-var _               = require("underscore")
-var UploadManager   = require("./lib/files/UploadManager.js")
 var logger          = require("./lib/util/Logger.js")
-var DBHelper        = require("./lib/db/DBHelper.js").DBHelper
-var gm              = require("gm")
 var fs              = require("fs")
-var im              = require("imagemagick")
-var SerialRunner    = require("serial").SerialRunner
-var uuid            = require("uuid")
-var crypto          = require("crypto")
 var AclHandler      = require("./lib/auth/AclHandler.js")
 var dust            = require("dustjs-linkedin")
 var Analytics       = require("./lib/web/Analytics.js")
-//var passport        = require('passport')
-//var LocalStrategy   = require('passport-local').Strategy;
+var RedisStore      = require("connect-redis")(express);
 
-
-var uploadManager = new UploadManager()
+require('express-namespace')
 
 var app = express()
 
 app.use(express.static(__dirname + '/static'))
+
 app.use(express.cookieParser("Arkhaios photography is gr34t"))
-//app.use(express.cookieSession({secret: "Arkhaios photography is gr34t"}))
-app.use(express.session({secret: "Arkhaios photography is gr34t"}))
+
+var redisConfig = getRedisConfig()
+
+if(redisConfig) {
+    app.use(express.session({
+        store: new RedisStore(redisConfig),
+        secret: "Arkhaios photography is gr34t"
+    }));
+} else {
+    logger.warn("Redis configuration not found. Falling back to in-memory sessions.")
+    app.use(express.session({secret: "Arkhaios photography is gr34t"}))
+}
 app.use(express.bodyParser())
 app.use(AclHandler.middleware())
 
 
 var compiledIndexPage
+
+function getRedisConfig() {
+    logger.debug("Redis config: HOST["+process.env.REDIS_HOST+"], PORT["+process.env.REDIS_PORT+"], PWD["+(process.env.REDIS_PWD ? "****":undefined)+"]")
+    if(process.env.REDIS_HOST &&
+       process.env.REDIS_PORT &&
+       process.env.REDIS_PWD) {
+
+        var conf = {
+            host: process.env.REDIS_HOST,
+            port: Number(process.env.REDIS_PORT),
+            pass: process.env.REDIS_PWD
+        }
+        console.log(JSON.stringify(conf))
+        return conf
+
+    }
+}
 
 app.get("/", function(req, res) {
 
@@ -54,12 +72,18 @@ app.get("/", function(req, res) {
     dust.render("index", data, function(err, rendered) {
         if(err) {
             logger.error(err)
-//            res.send({reason: err.message}, 500);
+            res.send({reason: "non public"}, 500);
         } else {
 //            res.contentType("text/html");
             res.send(rendered);
         }
-    });
+    })
+})
+
+
+app.get("/login", function(req, res) {
+    Analytics.trackPage("/login")
+    res.sendfile(__dirname + "/static/login.html")
 })
 
 app.get("/admin", function(req, res) {
@@ -70,560 +94,14 @@ app.get("/admin", function(req, res) {
     }
 })
 
-app.get("/login", function(req, res) {
-    Analytics.trackPage("/login")
-    res.sendfile(__dirname + "/static/login.html")
+app.namespace("/admin", require("./lib/webservices/Admin.js")(app))
+app.namespace("/auth", require("./lib/webservices/Auth.js")(app))
+app.namespace("/api", require("./lib/webservices/API.js")(app))
+
+
+
+var port = process.env.PORT || 3000
+
+app.listen(port, function() {
+    logger.info("Listening on port "+port)
 })
-
-app.post("/login", function(req, res) {
-
-    var password = req.param("password")
-
-    password = hash(password)
-
-    DBHelper.Config.findByKey("admin", {}, function(err, admin) {
-        if(err) {
-            res.send(err, 400)
-        } else if(admin) {
-
-            if(admin.password === password) {
-                req.session.admin = true
-                res.redirect("/admin")
-            } else {
-                req.session.admin = false
-                res.send({reason: "wrong password"}, 403)
-            }
-
-        } else {
-
-            admin = {
-                key: "admin",
-                password: password
-            }
-
-            DBHelper.Config.save(admin, function(err, admin) {
-                if(err) {
-                    res.send(err, 400)
-                } else {
-                    logger.info("First login. Administrator password created")
-                    req.session.admin = true
-                    res.redirect("/admin")
-                }
-            })
-        }
-    })
-})
-
-function hash(string) {
-    var sha512 = crypto.createHash("sha512")
-    sha512.update(string, "utf8")
-    return sha512.digest("base64")
-}
-
-app.get("/logout", function(req, res) {
-    Analytics.trackPage("/logout")
-    req.session.destroy()
-    res.redirect("/")
-})
-
-
-
-app.get("/auth/:aclUid", function(req, res) {
-
-    Analytics.trackPage("/auth/"+req.param("aclUid"))
-
-    if(req.cookies.acls) {
-        res.cookie("acls", req.cookies.acls.concat(req.param("aclUid")))
-    }
-
-    AclHandler.populateSessionWithAccessibleTags(req, function(err) {
-
-        if(err) {
-
-            logger.error(err)
-            res.send(err, 400)
-
-        } else {
-
-            logger.info("Authenticated with ACL ["+req.param("aclUid")+"]. Access to tags ["+JSON.stringify(req.session.tags)+"]")
-            res.redirect("/")
-        }
-    })
-})
-
-app.post("/api/acl", function(req, res) {
-
-    if(!req.session.admin) {
-        res.send({reason: "login required"}, 401)
-        return
-    }
-
-    var acl = {
-        uid: uuid.v4(),
-        name: req.param("name"),
-        tags: req.param("tags") ? req.param("tags").split(",") : []
-    }
-
-    DBHelper.ACL.save(acl, function(err, savedAcl) {
-
-        if(err) {
-            logger.error(err)
-            res.send(err, 400)
-        } else {
-            res.redirect("/admin")
-        }
-
-    })
-
-})
-app.post("/api/acl/:uid", function(req, res) {
-
-    if(!req.session.admin) {
-        res.send({reason: "login required"}, 401)
-        return
-    }
-
-    DBHelper.ACL.findByKey(req.param("uid"), {}, function(err, acl) {
-        if(err) {
-            logger.error(err)
-            res.send(err, 400)
-        } else if(acl) {
-            var hasChanged = false;
-
-            for(var attr in req.body) {
-                switch(attr) {
-                    case "uid":
-                        logger.warn("Attempt to change the UID of the acl is refused");
-                        break;
-                    case "tags":
-                        req.body[attr] = req.body[attr].split(",");
-                    default:
-                        if(!_.isEqual(acl[attr], req.body[attr])) {
-                            acl[attr] = req.body[attr];
-                            hasChanged = true;
-                        }
-                        break;
-                }
-            }
-
-            if(hasChanged) {
-                DBHelper.ACL.save(acl, function(err, updatedAcl) {
-                    if(err) {
-                        logger.error(err)
-                        res.send(err, 400)
-                    } else {
-                        res.redirect("/admin")
-                    }
-                })
-            } else {
-                res.redirect("/admin")
-            }
-        } else {
-            // TODO log error
-            res.redirect("/admin")
-        }
-    })
-
-})
-
-
-app.get("/api/acl/list", function(req, res) {
-
-    if(!req.session.admin) {
-        return res.send({reason: "login required"}, 401)
-    }
-
-    var from = req.param("from")
-    var to = req.param("to")
-
-    DBHelper.ACL.find(
-        {},
-        {
-            _id: 0,
-            uid: 1,
-            name: 1,
-            tags: 1
-        },
-        {
-            sort:   [["name", "asc"]]
-//            limit:  (to-from),
-//            skip:   from
-        },
-        function(err, acls) {
-
-            if(err) {
-                res.send(err, 400)
-            } else {
-                res.send(acls)
-            }
-
-        }
-    )
-
-})
-
-app.post("/api/upload", function(req, res) {
-
-    if(!req.session.admin) {
-        res.send({reason: "login required"}, 401)
-        return
-    }
-
-    logger.info("Received upload: "+JSON.stringify(req.files))
-
-    if(req.files) {
-        if(_.isArray(req.files.file)) {
-            var r = new SerialRunner()
-
-            for(var i = 0  ; i < req.files.file.length ; i++) {
-
-                r.add(uploadManager.upload, req.files.file[i])
-
-            }
-
-            var errors = []
-
-            r.onError(function(err) {
-                errors.push(err)
-            })
-
-            r.run(function() {
-                if(errors.length > 0) {
-                    res.send(errors, 400)
-                } else {
-                    res.redirect("/admin")
-                }
-            })
-
-        } else {
-            uploadManager.upload(req.files.file, function(err, imageInfo) {
-                if(err) {
-                    logger.error(err)
-                    res.send(err, 400)
-                } else {
-                    res.redirect("/admin")
-                }
-            })
-
-        }
-    } else {
-
-        logger.error("Could not find files to upload in the http request")
-        res.send({ "reason": "missing files to upload" }, 400)
-
-    }
-
-})
-
-app.get("/api/count/:tag", function(req, res) {
-
-    var query = {tags: {"$in": req.session.tags}}
-
-    if(req.session.admin) {
-        query = {}
-    }
-
-    DBHelper.Image.count(query, {}, function(err, count) {
-        if(err) {
-            res.send(err, 500)
-        } else {
-            res.send({count: count})
-        }
-    })
-})
-
-app.get("/api/list/:tag/:from/:to", function(req, res) {
-
-//    if(!req.session.tags) {
-//        if(req.cookies.acls) {
-//
-//        }
-//        res.cookie('rememberme', ["",""], { expires: new Date(Date.now() + 900000), httpOnly: true });
-//    }
-
-
-    var to = req.param("to")
-    var from = req.param("from")
-
-
-    var query = {tags: {"$in": req.session.tags}}
-
-    if(req.session.admin) {
-        query = {}
-    }
-
-    DBHelper.Image.find(
-    query,
-    {
-        _id: 0,
-        uid: 1,
-        name: 1,
-        width: 1,
-        height: 1,
-        tags: 1,
-        "dateUploaded": 1
-    },
-    {
-        sort:   [["dateUploaded", "desc"]],
-        limit:  (to-from),
-        skip:   from
-    },
-        function(err, imagesInfos) {
-
-            if(err) {
-                res.send(err, 400)
-            } else {
-                res.send(imagesInfos)
-            }
-
-        }
-    )
-
-})
-
-app.get("/api/info/:uid", function(req, res) {
-
-    // TODO: add ACL check
-
-    DBHelper.Image.findByKey(req.param("uid"), {}, function(err, imageInfo) {
-        if(err) {
-            logger.error(err)
-            res.send(err, 400)
-        } else {
-            var fs = require("fs")
-            logger.info("Found image at path ["+imageInfo.path+"]")
-            res.send(imageInfo)
-        }
-    })
-
-})
-
-app.post("/api/image/:uid", function(req, res) {
-    if(!req.session.admin) {
-        res.send({reason: "login required"}, 401)
-        return
-    }
-
-    DBHelper.Image.findByKey(req.param("uid"), {}, function(err, imageInfo) {
-        if(err) {
-            logger.error(err)
-            res.send(err, 400)
-        } else if(imageInfo) {
-            var hasChanged = false;
-
-            for(var attr in req.body) {
-                switch(attr) {
-                    case "uid":
-                        logger.warn("Attempt to change the UID of the image is refused");
-                        break;
-                    case "tags":
-                        req.body[attr] = req.body[attr].split(",");
-                    default:
-                        if(!_.isEqual(imageInfo[attr], req.body[attr])) {
-                            imageInfo[attr] = req.body[attr];
-                            hasChanged = true;
-                        }
-                        break;
-                }
-            }
-
-            if(hasChanged) {
-                DBHelper.Image.save(imageInfo, function(err, updatedImageInfo) {
-                    if(err) {
-                        logger.error(err)
-                        res.send(err, 400)
-                    } else {
-                        res.redirect("/admin")
-                    }
-                })
-            } else {
-                res.redirect("/admin")
-            }
-        } else {
-            // TODO log error
-            res.redirect("/admin")
-        }
-    })
-
-})
-
-function tagsMatch(tags1, tags2) {
-
-    var match = false
-
-    if(tags1 && tags2) {
-        for(var i = 0 ; i < tags1.length ; i++) {
-            if(tags2.indexOf(tags1[i]) !== -1) {
-                match = true
-                break
-            }
-        }
-    }
-
-    return match
-}
-
-app.delete("/api/image/:uid", function(req, res) {
-
-    if(!req.session.admin) {
-        res.send({reason: "login required"}, 401)
-        return
-    }
-
-    DBHelper.Image.findByKey(req.param("uid"), {}, function(err, imageInfo) {
-        if(err) {
-            logger.error(err)
-            res.send(err, 400)
-        } else if(imageInfo) {
-
-            var fs = require("fs")
-            logger.info("Found image at path ["+imageInfo.path+"]")
-
-            Analytics.trackEvent({
-                category: 'Image',
-                action: 'delete',
-                label: imageInfo.name,
-                value: req.param("uid")
-            })
-
-
-            logger.info("deleting image ["+imageInfo.uid+"]")
-            DBHelper.Image.remove({uid: imageInfo.uid}, function(err) {
-                if(err) {
-                    logger.error(err)
-                    res.send(err, 400)
-                } else {
-                    fs.unlink(imageInfo.path, function(err) {
-                        if(err) {
-                            logger.error(err)
-                            res.send(err, 400)
-                        } else {
-                            res.send(imageInfo)
-
-                            logger.info("deleted image ["+imageInfo.uid+"]")
-                        }
-                    })
-                }
-            })
-
-        } else {
-            res.send("Could not find image with UID ["+req.param("uid")+"]", 404)
-        }
-    })
-})
-
-app.get("/api/image/:uid", function(req, res) {
-
-//    Analytics.trackPage("/api/image/"+req.param("uid"))
-
-    DBHelper.Image.findByKey(req.param("uid"), {}, function(err, imageInfo) {
-        if(err) {
-            logger.error(err)
-            res.send(err, 400)
-        } else if(imageInfo) {
-
-            var fs = require("fs")
-            logger.info("Found image at path ["+imageInfo.path+"]")
-
-            if(!req.session.admin && !tagsMatch(req.session.tags, imageInfo.tags)) {
-                return res.send({"reason": "You are not authorized to see this image"}, 403)
-            }
-
-            var width = req.param("width")
-            var height = req.param("height")
-
-            if(width || height) {
-
-                Analytics.trackEvent({
-                    category: 'Image',
-                    action: 'small',
-                    label: imageInfo.name,
-                    value: req.param("uid")
-                })
-
-                if(!width) {
-                    if(imageInfo.width) {
-                        width = imageInfo.width
-                    } else {
-                        width = 5 * height;
-                    }
-                }
-                if(!height) {
-                    if(imageInfo.height) {
-                        height = imageInfo.height
-                    } else {
-                        height = 5 * width;
-                    }
-                }
-
-                logger.info("returning resized image with width="+width+", height="+height)
-
-                var format;
-
-                switch(imageInfo.type) {
-                    case "image/png":
-                        format = "png";
-                        break;
-                    case "image/jpg":
-                    case "image/jpeg":
-                    default:
-                        format = "jpg"
-                        break;
-                }
-
-                var resizeOptions = {
-                    srcPath: imageInfo.path,
-                    srcData: null,
-                    srcFormat: null,
-                    dstPath: undefined,
-                    quality: 1,
-                    format: format,
-                    progressive: false,
-                    width: width,
-                    height: height,
-                    strip: true,
-                    filter: 'Lagrange',
-                    sharpening: 0.2,
-                    customArgs: []
-                }
-
-                var gm = require("gm")
-
-                var stream = gm(imageInfo.path).resize(width, height).stream();
-
-                res.contentType(imageInfo.type);
-
-                stream.on("data", function(data) {
-                    res.write(data)
-                })
-
-                stream.on("end", function(data) {
-                    res.end();
-                })
-
-                stream.on("error", function() {
-                    res.send({reason: "unknown error while resizing the image"}, 500);
-                })
-
-            } else {
-
-                Analytics.trackEvent({
-                    category: 'Image',
-                    action: 'raw',
-                    label: imageInfo.name,
-                    value: req.param("uid")
-                })
-
-                res.setHeader("ContentType", "image/jpeg")
-                var readStream = fs.createReadStream(imageInfo.path);
-                readStream.pipe(res);
-            }
-
-        } else {
-            res.send("Could not find image with UID ["+req.param("uid")+"]", 404)
-        }
-    })
-
-})
-
-app.listen(process.env.PORT || 3000)
